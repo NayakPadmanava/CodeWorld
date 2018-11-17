fbe4df34f96f5093b113c48ac29f71ee4dada048
Padmanava Nayak <pnayak@navexglobal.com>
2018-11-17 11:58:11 +0530

    AbstractDemo

---
+++ AbstractDemo/AbstractDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDemo
+{
+    class Program:EncoderFallback
+    {
+        static void Main(string[] args)
+        {
+            Tshirt ob = new Tshirt();
+            ob.Discount();
+            Console.ReadLine();
+        }
+    }
+}
