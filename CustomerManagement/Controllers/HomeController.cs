c716b4066d3633039580cc62ffccb4e56064eb05
Padmanava Nayak <nayak.padma84@gmail.com>
2018-11-18 21:30:43 +0530

    Concept of ADO.NET Filters

---
+++ CustomerManagement/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomerManagement.Controllers
+{
+   // [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult About1()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View("About");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
