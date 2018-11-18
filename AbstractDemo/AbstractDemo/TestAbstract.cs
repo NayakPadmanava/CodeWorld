d6404e0f928ba9f8cc1d28c8f65ee5a2b604242f
Padmanava Nayak <nayak.padma84@gmail.com>
2018-11-18 21:16:36 +0530

    Assembly Demo details

---
+++ AbstractDemo/AbstractDemo/TestAbstract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDemo
+{
+  public abstract class TestAbstract
+    {
+        public virtual void Discount()
+        { }
+
+        public void Add()
+        {
+            Console.WriteLine("This is my normal method");
+
+        }
+    }
+
+    public class Tshirt : TestAbstract
+    {
+        //public override void Discount()
+        //{
+        //    Console.WriteLine("This is abstract impementation");
+            
+        //}
+
+    }
+}
---
+++ AssemblyDemo/AssemblyDemo/Sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyDemo
+{
+  public  class Sum
+    {
+        public int Add()
+        {
+            int i = 10, j = 20;
+            return (i + j);
+        }
+    }
+}
